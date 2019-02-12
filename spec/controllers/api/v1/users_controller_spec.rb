8816d11c4ac50aca1101efa0ee178405ebbbae38
fabio <fabiolopes.dev@gmail.com>
2019-02-12 16:18:55 -0300

    Refatorado página dos testes

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,13 +1,10 @@
 require 'spec_helper'
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 describe Api::V1::UsersController do
-  before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
-
   describe "GET #show" do
     before(:each) do
       @user = FactoryGirl.create :user
-      get :show, id: @user.id, format: :json
+      get :show, id: @user.id
     end
 
     it "returns the information about a reporter on a hash" do
@@ -19,43 +16,49 @@ describe Api::V1::UsersController do
   end
 
   describe "POST #create" do
-    context "when is sucessfully created" do
+
+    context "when is successfully created" do
       before(:each) do
         @user_attributes = FactoryGirl.attributes_for :user
-        post :create, { user: @user_attributes }, format: :json
+        post :create, { user: @user_attributes }
       end
 
       it "renders the json representation for the user record just created" do
-        user_response= json_response
+        user_response = json_response
         expect(user_response[:email]).to eql @user_attributes[:email]
       end
-      it { should respond_with 201 }
 
+      it { should respond_with 201 }
     end
 
     context "when is not created" do
       before(:each) do
-        @invalid_user_attributes = { password:"12345678", password_confirmation: "12345678" }
-        post :create, { user: @invalid_user_attributes }, format: :json
+        @invalid_user_attributes = { password: "12345678", password_confirmation: "12345678" } #notice I'm not including the email
+        post :create, { user: @invalid_user_attributes }
       end
 
       it "renders an errors json" do
-        user_response= json_response
+        user_response = json_response
         expect(user_response).to have_key(:errors)
-    end
-    it "renders the json errors on why the user could not be created" do
-      user_response = json_response
-      expect(user_response[:errors][:email]).to include "can't be blank"
-    end
-    it { should respond_with 422}
+      end
+
+      it "renders the json errors on whye the user could not be created" do
+        user_response = json_response
+        expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+
+      it { should respond_with 422 }
     end
   end
 
   describe "PUT/PATCH #update" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+    end
+
     context "when is successfully updated" do
       before(:each) do
-        @user = FactoryGirl.create :user
-        patch :update, { id: @user.id,user: { email: "newmail@example.com" } }, format: :json
+        patch :update, { id: @user.id, user: { email: "newmail@example.com" } }
       end
 
       it "renders the json representation for the updated user" do
@@ -68,9 +71,7 @@ describe Api::V1::UsersController do
 
     context "when is not created" do
       before(:each) do
-        @user = FactoryGirl.create :user
-        patch :update, { id: @user.id,
-                         user: { email: "bademail.com" } }, format: :json
+        patch :update, { id: @user.id, user: { email: "bademail.com" } }
       end
 
       it "renders an errors json" do
@@ -78,7 +79,7 @@ describe Api::V1::UsersController do
         expect(user_response).to have_key(:errors)
       end
 
-      it "renders the json errors on whey the user could not be created" do
+      it "renders the json errors on whye the user could not be created" do
         user_response = json_response
         expect(user_response[:errors][:email]).to include "is invalid"
       end
@@ -90,8 +91,10 @@ describe Api::V1::UsersController do
   describe "DELETE #destroy" do
     before(:each) do
       @user = FactoryGirl.create :user
-      delete :destroy,{id: @user.id}, format: :json
+      delete :destroy, { id: @user.id }
     end
-    it{ should respond_with 204}
+
+    it { should respond_with 204 }
+
   end
 end
