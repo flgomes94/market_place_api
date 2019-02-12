52fc36ee0f343e300c7c197ec68bc4f604abfaad
Fabio <fabiolopes.dev@gmail.com>
2019-02-12 01:26:34 -0300

    Setado config inicial da api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 MarketPlaceApi::Application.routes.draw do
-  
+  #api definition
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api'}, path: '/' do
+    #lista de recursos aqui
+
+  end
 end
