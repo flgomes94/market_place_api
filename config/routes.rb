4078b594340732567a2e870f1ab368a88e2588ae
Fabio <fabiolopes.dev@gmail.com>
2019-02-12 02:09:01 -0300

    Adicionando devise 'user' ao Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
+require 'api_constraints'
 MarketPlaceApi::Application.routes.draw do
+  devise_for :users
   #api definition
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api'}, path: '/' do
     #lista de recursos aqui
-
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default:true) do
+    end
   end
 end
