7af4f321769bfde92b7b101d10f72415b3e4b23e
fabio <fabiolopes.dev@gmail.com>
2019-02-12 13:56:08 -0300

    Atualizando controller e adicionando CSF token

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ require 'api_constraints'
 MarketPlaceApi::Application.routes.draw do
   devise_for :users
   # Api definition
-  namespace :api, defaults: { format: :json },
-                              constraints: { subdomain: 'api' }, path: '/'  do
+  namespace :api, defaults: { format: :json }  do
     scope module: :v1,
               constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
