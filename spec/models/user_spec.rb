fa8217909b4af2e86bb41c702f343e14c4481d44
fabio <fabiolopes.dev@gmail.com>
2019-02-12 11:30:47 -0300

    Adds user firsts specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,13 @@
 require 'spec_helper'
+
 describe User do
-  before { @user = FactoryGirl.build(:user)}
+  before { @user = FactoryGirl.build(:user) }
+
   subject { @user }
 
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-  it { should allow_value('example@domain.com').for(:email) }
 
   it { should be_valid }
-
-end
-
-describe "when wmail is not present" do
-  before { @user.email = " "}
-  it {should_note be_valid }
-
 end
